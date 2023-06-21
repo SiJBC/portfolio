74eb72c0f2f7442e90da383f7cd78589821ac8fa
Simon Colman <55180479+SiJBC@users.noreply.github.com>
2023-06-21 22:29:04 +1000

    env fix

--- src/app/components/postComponent.tsx
+++ src/app/components/postComponent.tsx
@@ -9,48 +9,54 @@ import 'swiper/css'
 
 export default function PostComponent () {
   const [posts, setPosts] = useState<any>()
+  const [loading, setLoading] = useState<boolean>(true)
   useEffect(() => {
     getAllWPPosts().then(res => {
       setPosts(res.data.posts.edges.map((el: any) => el.node))
+      setLoading(false)
     })
   }, [])
 
   return (
-    <Box
-      width={['100%', '75%']}
-      padding='6'
-      paddingInline={['6', '6', '24']}
-      margin={['0', '0', 'auto']}
-      bg='white'
-      boxShadow={['none', 'none', 'lg']}
-      gap='10px'
-    >
-      <Text
-        fontSize='2xl'
-        textAlign={['center', 'center', 'left']}
-        fontWeight='semibold'
-      >
-        Recent Posts
-      </Text>
-      <Swiper
-        spaceBetween={5}
-        slidesPerView={3.3}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={swiper => console.log(swiper)}
-      >
-        {posts &&
-          posts.map((post: any, i: number) => (
-            <React.Fragment key={`post- ${i}`}>
-              <SwiperSlide>
-                <Posts
-                  title={post.title}
-                  description={post.excerpt}
-                  href={`https://simoncolman.com/${post.slug}`}
-                ></Posts>
-              </SwiperSlide>
-            </React.Fragment>
-          ))}
-      </Swiper>
-    </Box>
+    <>
+      {!loading && (
+        <Box
+          width={['100%', '75%']}
+          padding='6'
+          paddingInline={['6', '6', '24']}
+          margin={['0', '0', 'auto']}
+          bg='white'
+          boxShadow={['none', 'none', 'lg']}
+          gap='10px'
+        >
+          <Text
+            fontSize='2xl'
+            textAlign={['center', 'center', 'left']}
+            fontWeight='semibold'
+          >
+            Recent Posts
+          </Text>
+          <Swiper
+            spaceBetween={5}
+            slidesPerView={3.3}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={swiper => console.log(swiper)}
+          >
+            {posts &&
+              posts.map((post: any, i: number) => (
+                <React.Fragment key={`post- ${i}`}>
+                  <SwiperSlide>
+                    <Posts
+                      title={post.title}
+                      description={post.excerpt}
+                      href={`https://simoncolman.com/$`}
+                    ></Posts>
+                  </SwiperSlide>
+                </React.Fragment>
+              ))}
+          </Swiper>
+        </Box>
+      )}
+    </>
   )
 }
